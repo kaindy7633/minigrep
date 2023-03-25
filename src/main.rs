fb64f4c56aac963aee0fe44f6d94cdfa5c88103c
liuzhen <3494688@qq.com>
2023-03-25 17:53:40 +0800

    更新main函数中的代码

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::{env, fs, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    // 对 build 返回的 `Result` 进行处理
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    println!("With text: \n{contents}");
+}
+
+struct Config {
+    _query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let _query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { _query, file_path })
+    }
 }
