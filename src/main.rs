3b149789ff2e4ec92433997ebcfe0bf087e80f42
kaindy7633 <kaindy7633@gmail.com>
2023-03-26 11:56:11 +0800

    更新练习代码，新增lib.rs库文件

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{env, fs, process};
+use std::{env, process};
+
+use minigrep::{run, Config};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,26 +11,8 @@ fn main() {
         process::exit(1);
     });
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With text: \n{contents}");
-}
-
-struct Config {
-    _query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let _query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { _query, file_path })
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
